ba9379505be48a9abb1638ced912c9a065034b0b
DaniilRybak <daniil_rus161@mail.ru>
2024-08-24 00:40:41 +0300

    phone/pc adp

--- public/scripts/scriptsLogin.js
+++ public/scripts/scriptsLogin.js
@@ -8,13 +8,15 @@ fetch('/isLoggedIn', {
         // Пользователь авторизован, отображаем кнопку
         document.getElementById('mapButton').style.display = 'block';
         document.getElementById('routesButton').style.display = 'block';
-        document.getElementById('settingsButton').style.display = 'block';
+        document.getElementById('OurInfo').style.display = 'none';
+        document.getElementById('ObrCall').style.display = 'none';
+        document.getElementById('Contact-info').style.display = 'none';
         document.getElementById('SignInAdmin').style.display = 'none';
+        document.getElementById('SignInAdmin1').style.display = 'none';
     } else {
         // Пользователь не авторизован, скрываем кнопку
         document.getElementById('mapButton').style.display = 'none';
         document.getElementById('routesButton').style.display = 'none';
-        document.getElementById('settingsButton').style.display = 'none';
         document.getElementById('SignInAdmin').style.display = 'block';
     }
 })
@@ -22,5 +24,7 @@ fetch('/isLoggedIn', {
     console.error('Ошибка проверки авторизации:', error);
     document.getElementById('mapButton').style.display = 'none';
     document.getElementById('routesButton').style.display = 'none';
-    document.getElementById('settingsButton').style.display = 'none';
+    document.getElementById('OurInfo').style.display = 'block';
+    document.getElementById('ObrCall').style.display = 'block';
+    document.getElementById('Contact-info').style.display = 'block';
 });
