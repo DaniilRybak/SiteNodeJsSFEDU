6f39aaf3da0b52358911a887baa256f0fb4d7c9c
DaniilRybak <daniil_rus161@mail.ru>
2024-10-17 00:12:16 +0300

    f

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 
 const secretKey = crypto.randomBytes(64).toString('hex');
 
-const hostname = '0.0.0.0';
+const hostname = 'ecotechbin.ru';
 const port = 3000; // Замените на желаемый порт
 
 // Настройка сессии
