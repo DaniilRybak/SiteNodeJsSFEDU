f1da8dcc3a13b391c825e799c635e003c485c83f
DaniilRybak <daniil_rus161@mail.ru>
2024-10-16 23:46:22 +0300

    f

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const session = require('express-session');
 
 const secretKey = crypto.randomBytes(64).toString('hex');
 
-const hostname = '79.174.85.236';
-const port = 99; // Замените на желаемый порт
+const port = 3000; // Замените на желаемый порт
 
 // Настройка сессии
 
@@ -234,6 +233,6 @@ app.put('/api/bins/:id/on_route', isLoggedIn, (req, res) => {
 });
 ////////////
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
   console.log('Server running');
 });
