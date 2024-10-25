ed5204a5c40b541d65fbdcb8ef44373ac06b1340
DaniilRybak <daniil_rus161@mail.ru>
2024-10-25 13:20:48 +0300

    poolMySql

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const crypto = require('crypto');
 const session = require('express-session');
 const winston = require('winston');
-
+const nodemailer = require('nodemailer');
 
 const secretKey = crypto.randomBytes(64).toString('hex');
 
 const hostname = '0.0.0.0';
-const port = 3000; // Замените на желаемый порт
+const port = 3000;
 
-// Настройка сессии
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'dresgeorg539@gmail.com',
+    pass: 'Primera12'
+  }
+});
 
 app.use(session({
   secret: secretKey,
@@ -19,16 +25,15 @@ app.use(session({
   saveUninitialized: false
 }));
 
-// Проверка авторизации для доступа к специальным функциям
 const isLoggedIn = (req, res, next) => {
   if (!req.session.username) {
-    return res.redirect('/'); 
+    return res.redirect('/');
   }
   next();
 };
 
 const logger = winston.createLogger({
-  level: 'info', // минимальный уровень логирования
+  level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.printf(({ level, message, timestamp }) => {
@@ -36,214 +41,202 @@ const logger = winston.createLogger({
     })
   ),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }), // логи ошибок в error.log
-    new winston.transports.File({ filename: 'combined.log' }), // все логи в combined.log
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
-
-// Настройки подключения к базе данных
 const dbConfig1 = {
   host: '79.174.88.253',
   user: 'Daniil',
   port: '15552',
   password: 'Primera12.',
-  database: 'trash_bins'
+  database: 'trash_bins',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 };
 
-const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  port: '3306',
-  password: '1234',
-  database: 'trash_bins'
-};
+const pool1 = mysql.createPool(dbConfig1);
 
-// Создание подключения к базе данных
-const connection = mysql.createConnection(dbConfig1);
-
-// Обработка ошибок подключения
-connection.connect(error => {
-  if (error) {
-    console.error('Ошибка подключения к базе данных: ' + error.stack);
-    return;
+async function keepAlive() {
+  try {
+    const connection = await pool1.getConnection();
+    await connection.query('SELECT 1');
+    connection.release();
+    console.log('Keep-alive запрос выполнен успешно');
+  } catch (error) {
+    console.error('Ошибка keep-alive запроса:', error.message);
   }
-  console.log('Подключение к базе данных успешно установлено');
+}
+
+setInterval(keepAlive, 60000);
+
+
+pool1.on('connection', connection => {
+    console.log(`Новое подключение: threadId ${connection.threadId}`);
+    connection.on('error', (err) => {
+        console.error('Pool1 connection error:', err.message);
+    });
 });
 
 app.use(express.static('public'));
-app.use(express.json()); // Для парсинга JSON в теле запроса
+app.use(express.json());
 
-// Главная страница с картой
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// API endpoint для получения данных о мусорных баках
-app.get('/api/bins', isLoggedIn, (req, res) => {
-  const query = 'SELECT * FROM bins';
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error('Ошибка получения данных из базы данных: ' + error.stack);
-      res.status(500).json({ error: 'Ошибка сервера' });
-      return;
-    }
+app.get('/api/bins', isLoggedIn, async (req, res) => {
+  try {
+    const connection = await pool1.getConnection();
+    const [results] = await connection.query('SELECT * FROM bins');
+    connection.release();
     res.json(results);
-  });
+  } catch (error) {
+    console.error('Ошибка получения данных из базы данных:', error.message);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
 });
 
-// вход
 app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/login.html');
 });
 
-// Обработка запроса на вход
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
-
-  // Получение хешированного пароля из базы данных
-  const query = 'SELECT password FROM users WHERE username = ?';
-  connection.query(query, [username], async (error, results) => {
-    if (error) {
-      console.error('Ошибка получения данных из базы данных: ' + error.stack);
-      res.status(500).json({ error: 'Ошибка сервера' });
-      return;
-    }
-
-    if (results.length === 0 || password != results[0].password) {
+  try {
+    const connection = await pool1.getConnection();
+    const [results] = await connection.execute('SELECT password FROM users WHERE username = ?', [username]);
+    if (!results.length || password !== results[0].password) {
       logger.error(`Неудачная попытка входа: ${req.ip} - ${username}`);
       res.status(401).json({ error: 'Неверный логин или пароль' });
-      return;
+    } else {
+      req.session.username = username;
+      res.json({ message: 'Успешный вход' });
     }
-
-    // Создание сессии для залогиненного пользователя
-    req.session.username = username;
-    res.json({ message: 'Успешный вход' });
-  });
+    connection.release();
+  } catch (error) {
+      console.error('Login Error: ', error.message)
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
 });
 
-// Защита специальных функций проверкой авторизации
 app.get('/map', isLoggedIn, (req, res) => {
-  
   res.sendFile(__dirname + '/map.html');
 });
 
-// Новый маршрут для проверки авторизации
 app.get('/isLoggedIn', (req, res) => {
   if (req.session.username) {
-    res.status(200).send(); // Отправляем пустой ответ, если пользователь авторизован
+    res.status(200).send();
   } else {
-    res.status(401).send(); // Отправляем ошибку 401, если пользователь не авторизован
+    res.status(401).send();
+  }
+});
+
+app.post('/api/bins', isLoggedIn, async (req, res) => {
+  const { latitude, longitude, serialNumber, fullness, chargeLevel } = req.body;
+  try {
+    const connection = await pool1.getConnection();
+    const [checkResults] = await connection.query('SELECT 1 FROM bins WHERE serial_number = ?', [serialNumber]);
+    if (checkResults.length > 0) {
+      return res.status(400).json({ error: 'Датчик с таким серийным номером уже существует' });
+    }
+    const [insertResults] = await connection.execute(
+      'INSERT INTO bins (latitude, longitude, serial_number, fullness, chargeLevel) VALUES (?, ?, ?, ?, ?)',
+      [latitude, longitude, serialNumber, fullness, chargeLevel]
+    );
+    connection.release();
+    res.status(201).json({ message: 'Данные успешно добавлены', id: insertResults.insertId });
+  } catch (error) {
+    console.error('Ошибка добавления данных', error.message)
+    res.status(500).json({ error: 'Ошибка сервера' });
   }
 });
 
 
-/////////////
-app.post('/api/bins', isLoggedIn, (req, res) => {
-  const { latitude, longitude, serialNumber, fullness, chargeLevel } = req.body;
-
-  // Проверка существования серийного номера
-  const checkQuery = `SELECT 1 FROM bins WHERE serial_number = ?`;
-  connection.query(checkQuery, [serialNumber], (checkError, checkResults) => {
-    if (checkError) {
-      console.error('Ошибка проверки серийного номера:', checkError);
-      return res.status(500).json({ error: 'Ошибка сервера' });
-    }
-
-    if (checkResults.length > 0) {
-      // Серийный номер уже существует
-      return res.status(400).json({ error: 'Датчик с таким серийным номером уже существует' });
-    }
-
-    // Если серийный номер уникален, добавляем датчик
-    const insertQuery = `INSERT INTO bins (latitude, longitude, serial_number, fullness, chargeLevel) VALUES (?, ?, ?, ?, ?)`;
-    connection.query(insertQuery, [latitude, longitude, serialNumber, fullness, chargeLevel], (insertError, insertResults) => {
-      if (insertError) {
-        console.error('Ошибка добавления данных:', insertError);
-        return res.status(500).json({ error: 'Ошибка сервера' });
-      }
-      res.status(201).json({ message: 'Данные успешно добавлены', id: insertResults.insertId });
-    });
-  });
-});
-//////////ОБНОВЛЕНИЕ МЕСТОПОЛОЖЕНИЯ
-
-app.get('/api/bins/:id', isLoggedIn, (req, res) => {
+app.get('/api/bins/:id', isLoggedIn, async (req, res) => {
   const binId = req.params.id;
-
-  const sql = `SELECT * FROM bins WHERE id = ?`;
-  connection.query(sql, [binId], (err, result) => {
-    if (err) {
-      console.error('Ошибка получения данных:', err);
-      return res.status(500).json({ message: 'Ошибка сервера' });
-    }
-
-    if (result.length === 0) {
+  try {
+    const connection = await pool1.getConnection();
+    const [result] = await connection.execute('SELECT * FROM bins WHERE id = ?', [binId]);
+    connection.release();
+    if (!result.length) {
       return res.status(404).json({ message: 'Контейнер не найден' });
     }
-
-    res.json(result[0]); // Возвращаем найденный контейнер
-  });
+    res.json(result[0]);
+  } catch (error) {
+    console.error('Ошибка получения данных:', error.message);
+    res.status(500).json({ message: 'Ошибка сервера' });
+  }
 });
 
-
-app.put('/api/bins/:id', isLoggedIn, (req, res) => {
+app.put('/api/bins/:id', isLoggedIn, async (req, res) => {
   const binId = req.params.id;
   const { latitude, longitude } = req.body;
-
-  // Валидация данных (необязательно, но рекомендуется)
   if (!latitude || !longitude) {
     return res.status(400).json({ message: 'Неверные данные' });
   }
-
-  const sql = `UPDATE bins SET latitude = ?, longitude = ? WHERE id = ?`;
-  connection.query(sql, [latitude, longitude, binId], (err, result) => {
-    if (err) {
-      console.error('Ошибка обновления данных:', err);
-      return res.status(500).json({ message: 'Ошибка сервера' });
-    }
+  try {
+    const connection = await pool1.getConnection();
+    const [result] = await connection.execute(
+      'UPDATE bins SET latitude = ?, longitude = ? WHERE id = ?',
+      [latitude, longitude, binId]
+    );
+    connection.release();
     if (result.affectedRows === 0) {
       return res.status(404).json({ message: 'Контейнер не найден' });
     }
     res.json({ message: 'Местоположение обновлено' });
-  });
-});
-/////////МАРШРУТ/////////////
-app.get('/api/bins/:id/on_route', isLoggedIn, (req, res) => {
-  const binId = req.params.id;
-
-  const sql = `SELECT * FROM bins WHERE id = ?`;
-  connection.query(sql, [binId], (err, result) => {
-    if (err) {
-      console.error('Ошибка получения данных:', err);
-      return res.status(500).json({ message: 'Ошибка сервера' });
-    }
-
-    if (result.length === 0) {
-      return res.status(404).json({ message: 'Контейнер не найден' });
-    }
-
-    res.json(result[0]); // Возвращаем найденный контейнер
-  });
+  } catch (error) {
+    console.error('Ошибка обновления данных:', error.message);
+    res.status(500).json({ message: 'Ошибка сервера' });
+  }
 });
 
-app.put('/api/bins/:id/on_route', isLoggedIn, (req, res) => {
-  const binId = req.params.id;
-  const { on_route } = req.body;
 
-  const sql = `UPDATE bins SET on_route = ? WHERE id = ?`;
-  connection.query(sql, [on_route, binId], (err, result) => {
-    if (err) {
-      console.error('Ошибка обновления данных:', err);
-      return res.status(500).json({ message: 'Ошибка сервера' });
+
+app.get('/api/bins/:id/on_route', isLoggedIn, async (req, res) => {
+    const binId = req.params.id;
+    try {
+        const connection = await pool1.getConnection();
+        const [rows] = await connection.execute('SELECT on_route FROM bins WHERE id = ?', [binId]);
+        connection.release();
+
+        if (!rows.length) {
+            return res.status(404).json({ message: 'Контейнер не найден' });
+        }
+        res.json({ on_route: rows[0].on_route });
+    } catch (error) {
+        console.error('Ошибка при получении статуса маршрута:', error.message);
+        res.status(500).json({ message: 'Ошибка сервера' });
     }
-    if (result.affectedRows === 0) {
-      return res.status(404).json({ message: 'Контейнер не найден' });
-    }
-    res.json({ message: 'Местоположение обновлено' });
-  });
 });
-////////////
+
+
+app.put('/api/bins/:id/on_route', isLoggedIn, async (req, res) => {
+    const binId = req.params.id;
+    const { on_route } = req.body;
+
+    try {
+        const connection = await pool1.getConnection();
+
+        const [result] = await connection.execute(
+            'UPDATE bins SET on_route = ? WHERE id = ?',
+            [on_route, binId]
+        );
+        connection.release();
+
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Контейнер не найден' });
+        }
+        res.json({ message: 'Статус маршрута обновлен' });
+    } catch (error) {
+        console.error('Ошибка при обновлении статуса маршрута:', error.message);
+        res.status(500).json({ message: 'Ошибка сервера' });
+    }
+});
 
 app.listen(port, hostname, () => {
   console.log('Server running');
